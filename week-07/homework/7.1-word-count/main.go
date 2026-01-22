16c19ceda0ebbab31dfe3ee0c28b54a5e2b453c1
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-22 20:19:10 +0500

    week-07: выполнено задание 7.1-word-count

--- week-07/homework/7.1-word-count/main.go
+++ week-07/homework/7.1-word-count/main.go
@@ -10,22 +10,49 @@ func WordCount(text string) map[string]int {
 	// TODO: реализуй функцию
 	// Используй strings.Fields для разбиения на слова
 	// Пройди по словам и увеличивай счётчик в map
-	_ = strings.Fields(text) // подсказка
-	return nil
+
+	txtSlice := strings.Fields(text)
+
+	m := make(map[string]int)
+	for _, v := range txtSlice {
+		_, ok := m[v]
+		if ok {
+			m[v] = m[v] + 1
+		} else {
+			m[v] = 1
+		}
+	}
+	return m
 }
 
 // CharCount возвращает map с количеством каждого символа
 func CharCount(text string) map[rune]int {
 	// TODO: реализуй функцию
 	// Пройди по строке с range — получишь rune
-	return nil
+	m := make(map[rune]int)
+	for _, v := range text {
+		m[v] = m[v] + 1
+	}
+
+	return m
 }
 
 // MostFrequent возвращает самое часто встречающееся слово
 func MostFrequent(text string) string {
 	// TODO: реализуй функцию
 	// Используй WordCount, затем найди слово с максимальным значением
-	return ""
+	m := WordCount(text)
+
+	maxCount := 0
+	key := ""
+	for k, v := range m {
+		if v > maxCount {
+			maxCount = v
+			key = k
+		}
+	}
+
+	return key
 }
 
 func main() {
