fa24d6b621a371af73e6e61e0fe2ae0c98a105c8
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-23 16:39:39 +0500

    week-07: выполнено задание 7.3-grouping

--- week-07/homework/7.3-grouping/main.go
+++ week-07/homework/7.3-grouping/main.go
@@ -13,14 +13,24 @@ func GroupByGrade(students []Student) map[int][]Student {
 	// TODO: реализуй функцию
 	// Создай map[int][]Student
 	// Пройди по студентам и добавляй в соответствующий слайс
-	return nil
+	m := make(map[int][]Student)
+	for _, v := range students {
+		m[v.Grade] = append(m[v.Grade], v)
+	}
+
+	return m
 }
 
 // GroupByFirstLetter группирует слова по первой букве
 func GroupByFirstLetter(words []string) map[rune][]string {
 	// TODO: реализуй функцию
 	// Для получения первой буквы используй []rune(word)[0]
-	return nil
+	m := make(map[rune][]string)
+	for _, v := range words {
+		firstLetter := []rune(v)[0]
+		m[firstLetter] = append(m[firstLetter], v)
+	}
+	return m
 }
 
 // GetStudentNames возвращает имена студентов указанного класса
@@ -28,7 +38,13 @@ func GetStudentNames(groups map[int][]Student, grade int) []string {
 	// TODO: реализуй функцию
 	// Получи слайс студентов по grade
 	// Извлеки только имена
-	return nil
+	students := groups[grade]
+
+	var names []string
+	for _, v := range students {
+		names = append(names, v.Name)
+	}
+	return names
 }
 
 func main() {
