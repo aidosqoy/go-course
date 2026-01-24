1f69bfc7cff534fa5d8c4a10ef7e22ecd07e34c8
Islam Uzaqpai <166946427+islamuzaqpai@users.noreply.github.com>
2026-01-24 17:31:44 +0500

    week-09: выполнено задание 9.2-bank-account

--- week-09/homework/9.2-bank-account/main.go
+++ week-09/homework/9.2-bank-account/main.go
@@ -15,35 +15,65 @@ type Account struct {
 func NewAccount(owner string, initial float64) *Account {
 	// TODO: реализуй функцию
 	// Создай и верни указатель на Account с заполненными полями
-	return &Account{}
+	return &Account{
+		Owner:   owner,
+		Balance: initial,
+	}
 }
 
 // Deposit пополняет счёт на указанную сумму
 func (a *Account) Deposit(amount float64) error {
 	// TODO: реализуй метод
 	// Проверь, что amount > 0
-	return errors.New("not implemented")
+	if amount < 0 {
+		return errors.New("invalid amount")
+	}
+
+	a.Balance = a.Balance + amount
+	return nil
 }
 
 // Withdraw снимает деньги со счёта
 func (a *Account) Withdraw(amount float64) error {
 	// TODO: реализуй метод
 	// Проверь, что amount > 0 и достаточно средств
-	return errors.New("not implemented")
+	if amount < 0 {
+		return errors.New("invalid amount")
+	}
+
+	if amount > a.Balance {
+		return errors.New("invalid amount")
+	}
+
+	a.Balance = a.Balance - amount
+	return nil
 }
 
 // Transfer переводит деньги на другой счёт
 func (a *Account) Transfer(to *Account, amount float64) error {
 	// TODO: реализуй метод
 	// Используй Withdraw и Deposit
-	return errors.New("not implemented")
+	if amount < 0 {
+		return errors.New("invalid amount")
+	}
+
+	err := a.Withdraw(amount)
+	if err != nil {
+		return fmt.Errorf("fail: %w", err)
+	}
+
+	err = to.Deposit(amount)
+	if err != nil {
+		return fmt.Errorf("fail: %w", err)
+	}
+	return nil
 }
 
 // Statement возвращает выписку по счёту
 func (a Account) Statement() string {
 	// TODO: реализуй метод
 	// Формат: "Счёт: {Owner}, Баланс: {Balance:.2f}"
-	return ""
+	return fmt.Sprintf("Счёт: %s Баланс: %.2f", a.Owner, a.Balance)
 }
 
 func main() {
